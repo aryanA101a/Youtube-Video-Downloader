dd8fc588448ed0e7d125a15a26766bfd881e7bf3
ashvinimore95 <more.ashvini1995@gmail.com>
2020-10-04 00:15:05 +0530

    audio downloader

--- GUI - YouTube Video Downloader - Tkinter/downloadLogic.py
+++ GUI - YouTube Video Downloader - Tkinter/downloadLogic.py
@@ -1,20 +1,36 @@
 from pytube import *
-vid=''
 
-def downloadLogic(url,path):
-# Inputs the URL
+vid = ''
+
+
+def downloadLogic(url, path):
+    # Inputs the URL
     video_url = url
 
-# Passing the url in the pytube function, that is YouTube
+    # Passing the url in the pytube function, that is YouTube
     yt = YouTube(video_url)
 
-# Here you have to choose any one format of the stream (format contains mime_type, resolution, fps, vcodec, acodec). Also, you can change it if you want but I've chosen the first format.
+    # Here you have to choose any one format of the stream (format contains mime_type, resolution, fps, vcodec, acodec). Also, you can change it if you want but I've chosen the first format.
     video = yt.streams.first()
-    
-    
 
-# Downloads the .mp4 file into the current directory. You can change it if you want.
+    # Downloads the .mp4 file into the current directory. You can change it if you want.
     video.download(path)
-    fs=str(round(video.filesize/(1024*1024))) + 'MB'
+    fs = str(round(video.filesize / (1024 * 1024))) + 'MB'
     return fs
-    
+
+
+def downloadAudioLogic(url, path):
+    # Inputs the URL
+    video_url = url
+
+    # Passing the url in the pytube function, that is YouTube
+    yt = YouTube(video_url)
+
+    # Here you have to choose any one format of the stream (format contains mime_type, resolution, fps, vcodec, acodec). Also, you can change it if you want but I've chosen the first format.
+    video = yt.streams.filter(only_audio=True).first()
+
+    # Downloads the .mp4 file into the current directory. You can change it if you want.
+    video.download(path)
+    fs = str(round(video.filesize / (1024 * 1024))) + 'MB'
+    return fs
+
--- GUI - YouTube Video Downloader - Tkinter/gui.py
+++ GUI - YouTube Video Downloader - Tkinter/gui.py
@@ -1,9 +1,9 @@
-from tkinter import filedialog,messagebox
+from tkinter import filedialog, messagebox
 from tkinter import *
-from downloadLogic import downloadLogic
+from downloadLogic import downloadLogic, downloadAudioLogic
 from tkinter.ttk import *
-bluegrey='#192734'
 
+bluegrey = '#192734'
 
 window = Tk()
 
@@ -12,75 +12,80 @@ window.title("Youtube Video Downloader")
 window.geometry('500x200')
 window.configure(bg=bluegrey)
 
-#Create style object
+# Create style object
 sto = Style()
 
-#configure style
-sto.configure('W.TButton', font= ('Roboto', 10, 'bold'),
-foreground='White',background='#FF304E')
+# configure style
+sto.configure('W.TButton', font=('Roboto', 10, 'bold'),
+              foreground='White', background='#FF304E')
 
+lbl = Label(window, text="Youtube Video URL", background=bluegrey, foreground='White')
 
+lbl.place(relx=.03, rely=.1, anchor=NW)
 
+txt = Entry(window, width=25)
 
+txt.place(relx=.5, rely=.1, anchor=N)
 
-lbl = Label(window, text="Youtube Video URL",background=bluegrey,foreground='White')
+lbl1 = Label(window, text="Download Location", background=bluegrey, foreground='White')
 
-lbl.place(relx =.03,rely=.1, anchor = NW) 
+lbl1.place(relx=.03, rely=.3, anchor=NW)
 
-txt = Entry(window,width=25)
+txt1 = Entry(window, width=25)
 
-txt.place(relx =.5,rely=.1, anchor = N) 
+txt1.place(relx=.5, rely=.3, anchor=N)
 
+lbl2 = Label(window, text="FileSize:", background=bluegrey, foreground='White')
 
-lbl1 = Label(window, text="Download Location",background=bluegrey,foreground='White')
+lbl2.place(relx=.03, rely=.5, anchor=NW)
 
-lbl1.place(relx =.03,rely=.3, anchor = NW) 
-
-txt1 = Entry(window,width=25)
-
-txt1.place(relx =.5,rely=.3, anchor = N) 
-
-lbl2 = Label(window, text="FileSize:",background=bluegrey,foreground='White')
-
-lbl2.place(relx =.03,rely=.5, anchor = NW) 
 
 # lbl3 = Label(window, text="Status: ",background=bluegrey,foreground='White')
 
-# lbl3.place(relx =.38,rely=.5, anchor = N) 
-
+# lbl3.place(relx =.38,rely=.5, anchor = N)
 
 
 def pathAssignment():
     root = Tk()
     root.withdraw()
     folder_selected = filedialog.askdirectory()
-    txt1.delete(0,END)
-    txt1.insert(0,folder_selected)
+    txt1.delete(0, END)
+    txt1.insert(0, folder_selected)
     root.destroy()
     return
 
-def pathHolder():
 
+def pathHolder():
     path = txt1.get()
     return path
 
+
 def clicked():
-    
     url = txt.get()
-    path=pathHolder()
-    FileSize=downloadLogic(url,path)
-    lbl2.configure(text='FileSize: '+FileSize)
-    messagebox.showinfo('Downloaded!', 'Video has been saved to '+path)
-    
+    path = pathHolder()
+    FileSize = downloadLogic(url, path)
+    lbl2.configure(text='FileSize: ' + FileSize)
+    messagebox.showinfo('Downloaded!', 'Video has been saved to ' + path)
 
 
-btn = Button(window, text="Download",style='W.TButton',command=clicked,width=20)
+def AudioClicked():
+    url = txt.get()
+    path = pathHolder()
+    FileSize = downloadAudioLogic(url, path)
+    lbl2.configure(text='FileSize: ' + FileSize)
+    messagebox.showinfo('Downloaded!', 'Audio has been saved to ' + path)
 
-btn.place(relx =.5,rely=.8, anchor = CENTER) 
 
-btn1 = Button(window, text="Browse",style='W.TButton', command=pathAssignment)
+btn = Button(window, text="Download", style='W.TButton', command=clicked, width=10)
+btn.place(relx=.5, rely=.8, anchor=NE)
 
-btn1.place(relx =.95,rely=.3, anchor = NE) 
+btn2 = Button(window, text="Download Audio", style='W.TButton', command=AudioClicked, width=15)
 
-window.resizable(0,0)
+btn2.place(relx=.5, rely=.8, anchor=NW)
+
+btn1 = Button(window, text="Browse", style='W.TButton', command=pathAssignment)
+
+btn1.place(relx=.95, rely=.3, anchor=NE)
+
+window.resizable(0, 0)
 window.mainloop()
