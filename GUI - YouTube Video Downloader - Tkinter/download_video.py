d862adaf442a0f172954f37dc2e6a038ca0f8c72
karla.brein <karla@brein.pe>
2020-10-04 20:36:14 -0500

    :sparkles: Add progress bar

--- GUI - YouTube Video Downloader - Tkinter/download_video.py
+++ GUI - YouTube Video Downloader - Tkinter/download_video.py
@@ -1,12 +1,15 @@
 from pytube import *
 vid=''
 
-def download_video(url,path):
+def download_video(url,path,progress):
+    def progress_function(stream, chunk, bytes_remaining):
+        size = video.filesize
+        progress_value = 100-bytes_remaining/size*100
+        progress.set(progress_value)
 # Inputs the URL
     video_url = url
-
 # Passing the url in the pytube function, that is YouTube
-    yt = YouTube(video_url)
+    yt = YouTube(video_url, on_progress_callback=progress_function)
 
 # Here you have to choose any one format of the stream (format contains mime_type, resolution, fps, vcodec, acodec). Also, you can change it if you want but I've chosen the first format.
     video = yt.streams.first()
